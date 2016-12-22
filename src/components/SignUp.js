66f1d1b07f6885ce54cc3d64cd166adece1931c1
casey <leptone55@gmail.com>
2016-12-22 00:12:02 -0800

    building out signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,15 +7,31 @@ class SignUp extends React.Component {
     }
     handleSubmit = e => {
         e.preventDefault()
-        const nameEle = e.target.querySelector('.name')
-        const langEle = e.target.querySelector('.lang')
-        const name = nameEle.value
-        const lang = langEle.value
-        nameEle.value = langEle.value = ''
+
+        const usernameElm = e.target.querySelector('#username')
+        const passwordElm = e.target.querySelector('#password')
+        const nativeLangElm = e.target.querySelector('#native-lang')
+        const learningElm = e.target.querySelector('#learning')
+        const skillLevelElm = e.target.querySelector('#skill-level')
+
+        const username = usernameElm.value
+        const password = passwordElm.value
+        const nativeLang = nativeLangElm.value
+        const learning = learningElm.value 
+        const skillLevel = skillLevelElm.value 
+
+        usernameElm.value = passwordElm.value = ''
+        learningElm.value = 'english'
+        skillLevelElm.value = '0'
+
         let payload = {
-            name,
-            lang
+            username,
+            password,
+            nativeLang,
+            learning,
+            skillLevel
         }
+
         const xhr = new XMLHttpRequest()
         xhr.open('post', '/user', true)
         xhr.setRequestHeader('Content-Type', 'application/json')
@@ -25,8 +41,36 @@ class SignUp extends React.Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input className='name' />
-                    <input className='lang'/>
+                    <label htmlFor='username'>Username:</label>
+                    <input id='username' type='text'/>
+
+                    <label htmlFor='password'>Password:</label>
+                    <input id='password' type='text'/>
+
+                    <label htmlFor='native-lang'>Native Language:</label>
+                    <select id='native-lang'>
+                        <option>English</option>
+                        <option>Spanish</option>
+                        <option>French</option>
+                    </select>
+
+                    <label htmlFor='learning'>Learning:</label>
+                    <select id='learning'>
+                        <option value='english'>English</option>
+                        <option value='spanish'>Spanish</option>
+                        <option value='french'>French</option>
+                    </select>
+
+                    <label htmlFor='skill-level'>Skill Level:</label>
+                    <select id='skill-level'>
+                        <option value='0'>Nothing - 0</option>
+                        <option value='1'>Toddler - 1</option>
+                        <option value='2'>Beginner Conversationalist - 2</option>
+                        <option value='3'>Intermediate Conversationalist - 3</option>
+                        <option value='4'>Advanced Conversationalist - 4</option>
+                        <option value='5'>Fluent - 5</option>
+                    </select>
+
                     <button>Submit</button>
                 </form>
             </div>
@@ -35,4 +79,3 @@ class SignUp extends React.Component {
 }
 
 export default SignUp
-                
