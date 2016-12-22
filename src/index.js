31d1f6e9a9ffeb0f05303e0a362268415c2a2f08
casey <leptone55@gmail.com>
2016-12-21 22:16:16 -0800

    added react-router

--- src/index.js
+++ src/index.js
@@ -1,56 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-//import io from 'socket.io-client'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
-class App extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = { users: [] }
-    }
-    handleSubmit = e => {
-        e.preventDefault()
-        const nameEle = e.target.querySelector('.name')
-        const langEle = e.target.querySelector('.lang')
-        const name = nameEle.value
-        const lang = langEle.value
-        nameEle.value = langEle.value = ''
-        let payload = {
-            name,
-            lang
-        }
-        const xhr = new XMLHttpRequest()
-        xhr.open('post', '/user', true)
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(JSON.stringify(payload))
-    }
-    handleClick = e => {
-        const xhr = new XMLHttpRequest()
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4  && xhr.status === 200 ) {
-                this.state.users = xhr.responseText
-                this.setState({ users: JSON.parse(xhr.responseText) })
-            }
-        }
-        xhr.open('get', '/user', true)
-        xhr.send()
-    }
-    render() {
-        const users = this.state.users.map((user, index) => {
-            return <div key={index}>{user.name} wants to learn {user.lang}</div>
-        })
-        return (
-            <div>
-                <h1>Lang Exchange</h1>
-                <p onClick= {this.handleClick}>Isnt it great!</p>
-                <form onSubmit={this.handleSubmit}>
-                    <input className='name' />
-                    <input className='lang'/>
-                    <button>Submit</button>
-                </form>
-                {users}
-            </div>
-        )
-    }
-}
+import App from './components/App'
+import Home from './components/Home'
+console.log('Home', Home)
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+render(
+    (
+        <Router history={hashHistory}>
+            <Route path='/' component={App}>
+                <IndexRoute component={Home} />
+            </Route>
+        </Router>
+    ),
+    document.getElementById('root')
+)
