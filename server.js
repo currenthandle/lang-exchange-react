273c2e573df630ccadced74b160fddcae8ca91cf
Ubuntu <leptone55@gmail.com>
2016-12-22 23:28:22 +0000

    remove socket.io for now

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongojs = require('mongojs')
 const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
-//const socketIo = require('socket.io')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackConfig = require('./webpack.config.js')
@@ -10,7 +9,6 @@ const webpackConfig = require('./webpack.config.js')
 const db = mongojs('lang-exchange', ['users'])
 const app = express()
 const server = http.createServer(app)
-//const io = socketIo(server)
 
 app.use(express.static(__dirname + '/public'))
 app.use(webpackDevMiddleware(webpack(webpackConfig)))
@@ -28,17 +26,5 @@ app.post('/user', (req, res) => {
         if(err) console.error('error',err)
     })
 })
-
-
-/*
-io.on('connection', socket => {
-    socket.on('message', body => {
-        socket.broadcast.emit('message', {
-            body,
-            from: socket.id.slice(8)
-        })
-    })
-})
-*/
 
 server.listen(3000) 
