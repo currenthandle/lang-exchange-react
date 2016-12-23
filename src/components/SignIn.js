ac2b3d97048f1ec9cd3d9e086d7ee155afdc1748
Ubuntu <leptone55@gmail.com>
2016-12-23 07:27:32 +0000

    starting login

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,13 +4,30 @@ class SignIn extends React.Component {
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='username'>Username:</label>
                     <input id='username' type='text' />
+                    <br/>
+                    <label htmlFor='password'>Password:</label>
                     <input id='password' type='password' />
+                    <br/>
+                    <button>Sign In</button>
                 </form>
             </div>
         )
     }
+    handleSubmit = e => {
+        e.preventDefault()
+
+        fetch('/login', {
+            method: 'post',
+            headers: new Headers({ 
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify()
+        })
+    }
+
 }
 
 export default SignIn
